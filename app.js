04409ae9baa95d7bfb0822ec5d6f58ffe328b288
Jame Sirichai <30431241+JAMEMSU@users.noreply.github.com>
2019-06-04 19:07:56 +0700

    Request QR

---
+++ app.js
@@ -0,0 +1,32 @@
+const axios = require('axios')
+const uniqid = require('uniqid')
+const date = new Date()
+
+let options = {
+   partnerTxnUid: 'PT'+uniqid.process(),
+   partnerId: 'PTR4459294',    // partnerId
+   partnerSecret: '47a06cf6ef3d42d1992536f84469e482', // partnerSecret
+   requestDt: date.toISOString(),
+   merchantId: 'KB142996853995', // merchantId
+   terminalId: 'term1',
+   qrType: '3',
+   txnAmount: 10.5,
+   txnCurrencyCode: 'THB',
+   reference1: 'INV001',
+   reference2: null,
+   reference3: null,
+   reference4: null,
+   metadata: 'ถุงผ้า 80.50, ดินสอ 20.00' 
+};
+
+
+
+async function main () {
+    let result = await axios.post('https://APIPORTAL.kasikornbank.com:12002/pos/qr_request',options)
+    return result
+}
+
+
+main().then((result)=>{
+    console.log(result.data)
+})
